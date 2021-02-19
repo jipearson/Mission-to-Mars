ec3321bf95e40530228303e63993bb8cefe6718d
Josh Pearson <jipearsonua@gmail.com>
2021-02-19 16:02:20 -0600

    adding module final

--- Mars_Scraping/app.py
+++ Mars_Scraping/app.py
@@ -1,5 +1,5 @@
 # Import tools
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scraping
 
@@ -23,7 +23,6 @@ def scrape():
    mars_data = scraping.scrape_all()
    mars.update({}, mars_data, upsert=True)
    return redirect('/', code=302)
-
 # Tell flask to run
 if __name__ == "__main__":
        app.run()
